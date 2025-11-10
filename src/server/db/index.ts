6c3b35b085ad30cff7541a9a7dbd8a01a359e704
wn55718 <wn55718@zut.edu.pl>
2025-11-10 16:15:54 +0100

    nvm

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,12 +1,18 @@
-// src/db.ts
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
-config({ path: ".env" }); // or .env.local
-
-
+import { env } from "~/env";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql, {schema});
+/**
+ * Cache the database connection in development. This avoids creating a new connection on every HMR
+ * update.
+ */
+const globalForDb = globalThis as unknown as {
+  conn: postgres.Sql | undefined;
+};
+
+const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+export const db = drizzle(conn, { schema });
