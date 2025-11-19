150200ad2fff288806e5f29dfbc0b899044b079f
Kacper Nowikiewicz <kacpereknowikiewicz2004@gmail.com>
2025-11-19 10:25:06 +0100

    Added hover effect to SignInButton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,11 @@ export default function RootLayout({
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
-              <SignInButton />
+              <SignInButton >
+                <button className="cursor-pointer">
+                  Sign In
+                </button>
+              </SignInButton>
               <SignUpButton>
                 <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                   Sign Up
