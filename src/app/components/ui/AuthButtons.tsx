7b2502e146df2704e9f362edba479f09515f1d12
Kacper Nowikiewicz <kacpereknowikiewicz2004@gmail.com>
2025-12-04 14:49:28 +0100

    changed AuthButtons to Polish

--- src/app/components/ui/AuthButtons.tsx
+++ src/app/components/ui/AuthButtons.tsx
@@ -4,7 +4,7 @@ export function SignInBtn() {
   return (
     <SignInButton mode='modal'>
       <button className="text-1xl cursor-pointer hover:text-[#5930d1]">
-        Sign In
+        Zaloguj się
       </button>
     </SignInButton>
   );
@@ -14,7 +14,7 @@ export function SignUpBtn() {
   return (
     <SignUpButton mode='modal'>
       <button className="text-1xl bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer hover:bg-[#5930d1] transition-colors">
-        Sign Up
+        Utwórz konto
       </button>
     </SignUpButton>
   );
