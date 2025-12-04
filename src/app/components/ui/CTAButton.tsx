d9cd3006b69df988bc860efa4a4536882a5afe66
Kacper Nowikiewicz <kacpereknowikiewicz2004@gmail.com>
2025-12-04 13:58:30 +0100

    added custom tilwind-css colors

--- src/app/components/ui/CTAButton.tsx
+++ src/app/components/ui/CTAButton.tsx
@@ -12,7 +12,7 @@ export function CTABtn({ action, label }: CTABtnProps) {
   const buttonClasses =
     action === 'sign-in'
       ? 'cursor-pointer hover:text-[#5930d1] font-medium text-sm sm:text-base'
-      : 'bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer hover:bg-[#5930d1] transition-colors';
+      : 'bg-secondary text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer hover:bg-[#5930d1] transition-colors';
 
   const ButtonContent = (
     <button className={buttonClasses}>
