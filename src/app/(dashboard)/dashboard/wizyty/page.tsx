2323587d3c3679275f4475619ff004fa20cc3bc8
blukasz739 <brylewskilukasz739@gmail.com>
2026-01-18 20:03:29 +0100

    simple select

--- src/app/(dashboard)/dashboard/wizyty/page.tsx
+++ src/app/(dashboard)/dashboard/wizyty/page.tsx
@@ -1,6 +1,16 @@
 import { auth, currentUser } from "@clerk/nextjs/server";
 import Link from "next/link";
+import { db } from "~/server/db/index";
+import { visits } from "~/server/db/schema";
+import {pets} from "~/server/db/schema";
 export default async function wizyty() {
+    const pett_1=await db.select({
+        
+    })
+    const result=await db.select({
+    field1:visits.visitDate
+    }).from(visits);
+    const field1= result[0]?.field1;
     const {isAuthenticated}=await auth()
     if(!isAuthenticated){
         return <div>Sign in to view this DashboardPage</div>
@@ -10,6 +20,7 @@ export default async function wizyty() {
     <main>
         <section>
             <h1>Twoje wizyty {user?.firstName}</h1>
+            <p>{field1}</p>
             <Link href={"/dashboard/wizyty/rejestracja_wizyt"}>
             <p>Zarejestruj wizyte!!!</p>
             </Link>
