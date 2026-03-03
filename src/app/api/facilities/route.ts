f2425d45e46e38c57ceaa374a281546e47357316
juli1o <ho55650@zut.edu.pl>
2026-03-03 14:58:40 +0100

    Usunięcie

--- src/app/api/facilities/route.ts
+++ src/app/api/facilities/route.ts
@@ -1,4 +1,4 @@
-import { type NextRequest } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 import { db } from '~/server/db';
 import { facilities } from '~/server/db/schema';
 import { eq, and } from 'drizzle-orm';
@@ -17,15 +17,9 @@ export async function GET(req: NextRequest) {
     .select()
     .from(facilities)
     .where(filters.length > 0 ? and(...filters) : undefined);
-    // problem z serializacją biginta
-//   return new Response(JSON.stringify(data, (_key, value) =>
-//     typeof value === "bigint" ? value.toString() : value
-// ), {
-//   headers: { "Content-Type": "application/json" },
-// });
+    
   return NextResponse.json(data, {
   status: 200,
-  // Replacer musi zwracać unknown, aby uciszyć lintera
   headers: {
     "Content-Type": "application/json",
   },
