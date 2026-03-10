c6ba8ef34cf85f23d461cd8f0ffcf65e05a65453
wn55718 <wn55718@zut.edu.pl>
2026-03-10 01:37:54 +0100

    d by d

--- src/lib/push.ts
+++ src/lib/push.ts
@@ -5,7 +5,7 @@ import { eq } from "drizzle-orm";
 
 webpush.setVapidDetails(
   "mailto:admin@example.com",
-  process.env.VAPID_PUBLIC_KEY!,
+  process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!,
   process.env.VAPID_PRIVATE_KEY!
 );
 
