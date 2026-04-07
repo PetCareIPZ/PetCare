938068a5ab9c011a4f25604261f9ee5e3e6e6ddf
Mikolaj <mikolaj@altcom.com.pl>
2026-04-07 15:24:53 +0200

    attach names to functions

--- src/app/(dashboard)/dashboard/loading.tsx
+++ src/app/(dashboard)/dashboard/loading.tsx
@@ -1,4 +1,4 @@
-export default function(){
+export default function loader(){
     return <div className="flex items-center justify-center h-64">
         <img src="/svg/loading.svg" alt="Loading . . ." width={100} height={100}/>
     </div>
